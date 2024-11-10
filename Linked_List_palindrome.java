9ed04aba122a28996eada14512f906091fcd76c5
Degamie_sign <106448560+degamie@users.noreply.github.com>
2024-11-10 18:37:26 +0530

    Update Linked_List_palindrome.java

--- Linked_List_palindrome.java
+++ Linked_List_palindrome.java
@@ -1,22 +1,20 @@
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ListNode slow=head,fast=head;
-        while(fast!=null && fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
+    //  S(C(N)==O(1)) and T(C(N)==O(N)) as it requires iterative searching in at each Data element
+        ListNode slow=head,fast=head;//Slow and fast Pointer Nodes Declare
+        while(fast!=null && fast.next!=null){//iterating throught LL's End Node
+            slow=slow.next;fast=fast.next.next;//Slow and Fast Pointers decalew
         }
-        ListNode prev=null;
-        while(slow!=null){
-            ListNode tmp=slow.next;
-            slow.next=prev;
-            prev=slow;
-            slow=tmp;
+        ListNode prev=null;//Initialize the Prev Node 
+        while(slow!=null){//iterating through Slow Pointer
+            ListNode tmp=slow.next;//Temp Node's Declare
+            slow.next=prev;//Slow and Prev Pointer Val Declare
+            prev=slow;slow=tmp;//Swapping the Prev and Slow Pointer
         }
-        while(prev!=null){
-            if(head.val!=prev.val)return false;
-                head=head.next;
-                prev=prev.next;
+        while(prev!=null){//iterating through Slow Pointer
+            if(head.val!=prev.val)return false;//Prininting False for Non Palidnrome LinkedList
+                head=head.next;prev=prev.next;//Swapping The Prev and head Nodes
         }
-        return true;
+        return true;//Palindrom linked List resultant True val Printing
     }
 }
